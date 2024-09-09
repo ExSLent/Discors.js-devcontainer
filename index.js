c82bc20a063e44ca5236cdd04d8c7650a5be9aa8
Alexis LEBRUN <alexis@informalex.be>
2024-09-09 17:41:25 +0000

    clean file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Require the necessary discord.js classes
+//Import dependecies
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
@@ -10,7 +10,29 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 //Command handlers
 client.commands = new Collection();
 
-// Import /comands files
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+// Import /commands files
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -29,9 +51,8 @@ for (const folder of commandFolders) {
 	}
 }
 
+
 // When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
