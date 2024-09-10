31254b6eb181e62e8ce24af25bd504d29a3e9493
Alexis LEBRUN <alexis@informalex.be>
2024-09-10 19:11:58 +0000

    update  index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+// Import Discord.JS library
 const Discord = require('Discord.js');
-const bot = new Discord.Client({intents: 53608447});
+// Create a bot instance
+const intents = new Discord.IntentsBitField(53608447);
+const bot = new Discord.Client({ intents: intents });
+// Import data fom config file
 const config = require('./config');
+// Import command loader
+const loadCommands = require('./loader/loadCommands.js');
 
-bot.login(config.token)
+//Connect the BOT
+bot.login(config.token);
+
+loadCommands(bot);
+
+//Log when the BOT is online & reday
+bot.on('ready', async (stream) => {
+  console.log(bot.user.tag + ' est en ligne');
+});
