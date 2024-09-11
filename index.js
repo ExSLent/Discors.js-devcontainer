04337dc2d79b257af64d877b7a73ec3dbfe9dc56
Alexis LEBRUN <alexis@informalex.be>
2024-09-11 19:04:42 +0000

    add comment

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ const Discord = require('discord.js');
 // Create a bot instance
 const intents = new Discord.IntentsBitField(53608447);
 const bot = new Discord.Client({ intents: intents });
-
 // Import data fom config file
 const config = require('./config');
 // Import command loader
 const loadCommands = require('./loader/loadCommands.js');
 
-//Connect the BOT
+// Connect the BOT
 bot.login(config.token);
 
+// Create commands collection
 bot.commands = new Discord.Collection();
+// Load commands
 try {
   loadCommands(bot);
 } catch (error) {
   console.error('Error while loading commands : ', error);
 }
 
-//Log when the BOT is online & reday
+// Log when the BOT is online & reday
 bot.on('ready', async (stream) => {
-  console.log(bot.user.tag + ' est en ligne');
+  console.log(bot.user.tag + ' is online');
 });
