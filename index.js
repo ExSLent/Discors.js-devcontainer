425ee1ca2a504933c93f626a40ef315818830c46
Alexis LEBRUN <alexis@informalex.be>
2024-09-11 19:02:41 +0000

    add try catch while loading commands

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Import Discord.JS library
-const Discord = require('Discord.js');
+const Discord = require('discord.js');
 // Create a bot instance
 const intents = new Discord.IntentsBitField(53608447);
 const bot = new Discord.Client({ intents: intents });
+
 // Import data fom config file
 const config = require('./config');
 // Import command loader
@@ -12,7 +13,11 @@ const loadCommands = require('./loader/loadCommands.js');
 bot.login(config.token);
 
 bot.commands = new Discord.Collection();
-loadCommands(bot);
+try {
+  loadCommands(bot);
+} catch (error) {
+  console.error('Error while loading commands : ', error);
+}
 
 //Log when the BOT is online & reday
 bot.on('ready', async (stream) => {
